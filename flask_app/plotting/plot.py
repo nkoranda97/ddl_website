2c96353199e16267986af35fb95f35c6d497ea4f
nkoranda97 <114971516+nkoranda97@users.noreply.github.com>
2024-09-24 08:48:38 -0700

    added hover to bar graph

--- flask_app/plotting/plot.py
+++ flask_app/plotting/plot.py
@@ -1,6 +1,6 @@
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, HoverTool
 from bokeh.palettes import Turbo256
-from bokeh.plotting import figure
+from bokeh.plotting import figure, show
 from bokeh.transform import factor_cmap
 import pandas as pd
 
@@ -8,32 +8,31 @@ def bar(df, x, title=''):
     
     df[x] = df[x].astype(str)
     group = df.groupby(x).size().reset_index(name='counts')
-    group = group.sort_values(by='counts', ascending=False)
-
-    x_cmap = factor_cmap(x, palette=Turbo256, factors=sorted(df[x].unique()))
     
-    # Create a ColumnDataSource
+    group = group.sort_values(by='counts', ascending=False)
+    
+    x_cmap = factor_cmap(x, palette=Turbo256, factors=group[x].tolist())
+    
     source = ColumnDataSource(group)
     
-    # Create a horizontal bar plot
     p = figure(y_range=group[x].astype(str), title=title,
-               toolbar_location=None, tools="")
+               toolbar_location="above", tools="pan,wheel_zoom,box_zoom,reset")
     p.hbar(y=x, right='counts', height=0.9, source=source, line_color=x_cmap, fill_color=x_cmap)
+    
+    hover = HoverTool()
+    hover.tooltips = [("Category", f"@{x}"), ("Count", "@counts")]
+    p.add_tools(hover)
     
     p.ygrid.grid_line_color = None
     p.x_range.start = 0
 
     return p
 
-# Example usage
 if __name__ == "__main__":
-    # Create a sample DataFrame
     
     df = pd.read_csv('/Users/nick/dandelion_tutorial/ddl_test.csv')
     
-    # Generate the bar plot
     plot = bar(df,'v_call_VDJ')
     
-    # To show the plot in a standalone script, you can still call show(plot)
     from bokeh.io import show
     show(plot)
